57b9232affaa3d1c72f289bdbc6b845c69d0db82
forst09 <dforst09@gmail.com>
2023-03-18 21:26:56 +0300

    add works

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+
     //close mobile menu
     let close = document.getElementById('closeMobile');
     if (close) {
@@ -17,10 +18,40 @@ document.addEventListener('DOMContentLoaded', function () {
             mobileMenu[0].classList.remove('active');
             document.querySelector('body').classList.remove('not-scroll');
         });
-    }
+    };
+
+
+
+    // const worksGallery = document.getElementById('works');
+    // const certificateGallery = document.getElementById('certificate');
+
+    // worksGallery.addEventListener('lgBeforeOpen', function () {
+    //     document.querySelector('body').classList.add('not-scroll');
+    // });
+    // worksGallery.addEventListener('lgBeforeClose', function () {
+    //     document.querySelector('body').classList.remove('not-scroll');
+    // });
+
+    // function closeM(gallery) {
+    //     const worksGallery = document.getElementById(gallery);
+    //     worksGallery.addEventListener('lgBeforeOpen', function () {
+    //         document.querySelector('body').classList.add('not-scroll');
+    //     });
+    //     worksGallery.addEventListener('lgBeforeClose', function () {
+    //         document.querySelector('body').classList.remove('not-scroll');
+    //     });
+    // }
+    // closeM('works');
 
     //open certificate
     lightGallery(document.getElementById('animated-thumbnails'));
+
+    //open works
+    lightGallery(document.getElementById('works'), {
+        plugins: [lgZoom],
+        selector: '.works__img',
+        showZoomInOutIcons: true
+    });
 });
 
 
